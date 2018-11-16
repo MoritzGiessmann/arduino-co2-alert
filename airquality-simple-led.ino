78de8024839c73b7bad06d0e4de808eeb35b65d4
Moritz Giessmann <moritz.giessmann@1und1.de>
2018-11-16 15:29:25 +0100

    change readme

--- airquality-simple-led.ino
+++ airquality-simple-led.ino
@@ -1,42 +1,6 @@
 /*
   Arduino Climate CO2 Alert
-  Warns you when CO2 levels are too high with an alert LED.
-
-  Notice!
-  The sensors used for this project need some time to "warm up". They need to run for about 48 hours
-  once before you can expect useful results. After that they need some time to
-  adjust. (About 1-2 hours).
-  The sensors don't calculate the CO2 directly, but by interpolating it from other values.
-  The gathered values just an indicator it's time to open the window, not an exact measurement.
-
-  Parts required
-  - 1 Arduino UNO (others may work too)
-  - 1 CCS811 sensor (needs to get soldered to
-  - 1 BME280 sensor
-  - 1 LED, red
-
-  Libraries required -- install via Sketch -> Include library --> Manage libraries
-  - Adafruit_BME280
-  - Adafruit_CCS811
-
-  Wiring -- Left device, right Arduino
-  - LED PLUS (the longer wire) -> 13, LED MINUS ->GND
-  - BME280:
-  -- VIN -> POWER 3.3 (together with CCS811 VCC)
-  -- GDN -> POWER GND (together with CCS811 GND)
-  -- SCL -> A5 (together with CCS811 SCL)
-  -- SDA -> A4 (together with CCS811 SCA)
-  - CCS811:
-  -- VCC -> POWER 3.3 (together with BME280 VIN)
-  -- GDN -> POWER GND (together with BME280 GND)
-  -- SCL -> A5 (together with BME280 SCL)
-  -- SDA -> A4 (together with BME280 SCA)
-  -- WAK -> POWER GND (together with BME280 GND)
-
-  If everything is correct the LED should light up if the CO2 value is over 1000ppm. That means: it's time to open your window.
-  This program also shows all kinds of interesting values (temperature, air pressure, humidity and volatile organic compounds)
-  when using the Arduino Serial Monitor (Tools -> Serial Monitor).
-
+  @see README.md for wiring and further details
 */
 
 /**
